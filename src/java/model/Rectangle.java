fcf122b19431b0e1547922b0e40e2fcefa71941a
daothaovan <73456875+daothaovan@users.noreply.github.com>
2020-10-28 11:57:22 +0700

    Add files via upload

--- src/java/model/Rectangle.java
+++ src/java/model/Rectangle.java
@@ -1,1 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
 
+/**
+ *
+ * @author DAOTHAOVAN
+ */
+public class Rectangle {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int red;
+    private int green;
+    private int blue;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+}
